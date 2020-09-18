3e672fd3265895b884246a599ab462c81d928f6a
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-09-18 18:14:57 +0530

    Format improvements

--- lib/Backend/calcParser.dart
+++ lib/Backend/calcParser.dart
@@ -14,40 +14,55 @@ class CalcParser {
   ];
 
   List<String> addToExpression(String value) {
-    // Code for Square of Number.
-    if (value.contains(FixedValues.squareChar)) {
+    if (calculationString.length != 0) {
       int lastIndex = calculationString.length - 1;
       String lastChar = calculationString[lastIndex];
-      calculationString[lastIndex] = '$lastChar\u00B2';
-    }
-    // Code for add bracket after following functions.
-    else if (value.contains('sin') ||
-        value.contains('cos') ||
-        value.contains('tan') ||
-        value.contains('ln') ||
-        value.contains('log')) {
-      calculationString.add('$value(');
-    }
-    // Code for cube root
-    else if (value.contains(FixedValues.cubeRoot)) {
-      calculationString.add('\u00B3√');
-    } else if (value.contains(FixedValues.decimalChar)) {
-      calculationString.add('.');
-    }
-    // Code for +/- Option
-    else if (value.contains(FixedValues.changeSignChar)) {
-      setSign();
-    } else {
+      // Check if previous value is NOT an operator.
+      if (!(operations.contains(value) && lastChar.contains(value))) {
+        // Code for Square of Number.
+        if (value.contains(FixedValues.squareChar))
+          calculationString[lastIndex] = '$lastChar\u00B2';
+
+        // Code for add bracket after following functions.
+
+        else if ({'sin', 'cos', 'tan', 'ln', 'log'}.contains(value))
+          calculationString.add('$value(');
+
+        // Code for cube root
+        else if (value.contains(FixedValues.cubeRoot))
+          calculationString.add('\u00B3√');
+        else if (value.contains(FixedValues.decimalChar))
+          calculationString.add('.');
+
+        // Code for +/- Option
+        else if (value.contains(FixedValues.changeSignChar))
+          setSign();
+        else
+          calculationString.add(value);
+      }
+    } else if (!({
+      '+',
+      '*',
+      '/',
+      FixedValues.divisionChar,
+      FixedValues.multiplyChar,
+      ')',
+      FixedValues.capChar,
+      '%',
+      FixedValues.squareChar,
+      FixedValues.changeSignChar
+    }.contains(value))) {
+      // Check if only value present is an operator.
+
       calculationString.add(value);
     }
-
     return calculationString;
   }
 
   void setSign() {
     // Not very advanced but just a basic function to insert minus sign wherever possible
 
-    int lastChar = calculationString.length - 1;
+    int lastChar = calculationString.length - 1; // Get index of last char
     bool found = false;
     int i = 0;
 
@@ -55,15 +70,13 @@ class CalcParser {
     for (i = lastChar; i >= 0; i--) {
       if ({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
               .contains(calculationString[i]) ==
-          false) {
-        break;
-      }
+          false) break;
     }
 
     // check if i is not equal to last item in the array, meaning there are numbers beginning from the end.
     if (i != lastChar) {
       // pre-check if i is -1 and avoid run-time errors.
-      if (i != -1) {
+      if (i != -1)
         switch (calculationString[i]) {
           case '(-':
             calculationString.removeAt(i);
@@ -80,10 +93,9 @@ class CalcParser {
           default:
             calculationString.insert(i + 1, '(-');
         }
-      } else {
+      else
         // if it indeed equal to -1 then add sign directly as there are no operators at this point, only a number.
         calculationString.insert(i + 1, '-');
-      }
     } else {
       // executes if there is an operator from the end, gets last available operator in calculator string and insert a sign there.
       for (i = lastChar; i >= 0; i--) {
@@ -93,9 +105,7 @@ class CalcParser {
           break;
         }
       }
-      if (found) {
-        calculationString.insert(i + 1, '(-');
-      }
+      if (found) calculationString.insert(i + 1, '(-');
     }
   }
 
