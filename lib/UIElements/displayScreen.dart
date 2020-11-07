3b8757ddd8628164200d08b65248038e3f279cd9
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-11-07 10:26:59 +0530

    Improvements.

--- lib/UIElements/displayScreen.dart
+++ lib/UIElements/displayScreen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:charcode/charcode.dart' as charcode;
 import 'package:calculator_lite/UIElements/editText.dart';
-import 'package:auto_size_text/auto_size_text.dart';
+import 'package:auto_size_text_field/auto_size_text_field.dart';
 
-class DisplayScreen extends StatelessWidget {
+class DisplayScreen extends StatefulWidget {
   const DisplayScreen({
     @required this.calculationString,
     @required this.mainValue,
@@ -13,39 +13,7 @@ class DisplayScreen extends StatelessWidget {
   final double mainValue;
 
   @override
-  Widget build(BuildContext context) {
-    String mainValueStr = formatNumber(mainValue);
-    return Expanded(
-      flex: 2,
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-        child: Container(
-          alignment: Alignment.centerLeft,
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Text Widget for all calculations.
-              TextFieldCalc(
-                calculationString: calculationString,
-              ),
-              AutoSizeText(
-                mainValueStr,
-                maxLines: 1,
-                style: mainValueStyle(),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  TextStyle mainValueStyle() => TextStyle(
-        fontSize: 45.0,
-        letterSpacing: 1.0,
-        fontWeight: FontWeight.bold,
-      );
+  _DisplayScreenState createState() => _DisplayScreenState();
 
   static String formatNumber(double value) {
     if (value == -0) value = 0; // For Cos90 = -0
@@ -60,4 +28,57 @@ class DisplayScreen extends StatelessWidget {
 
     return valueStr.replaceAll(infinity, infinitySymbol);
   }
+}
+
+class _DisplayScreenState extends State<DisplayScreen> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    runInitial();
+    return Expanded(
+      flex: 2,
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        child: Container(
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Text Widget for all calculations.
+              TextFieldCalc(
+                calculationString: widget.calculationString,
+              ),
+              AutoSizeTextField(
+                controller: myController,
+                minLines: 1,
+                maxLines: 2,
+                readOnly: true,
+                showCursor: true,
+                decoration: null,
+                style: mainValueStyle(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void runInitial() {
+    myController.text = DisplayScreen.formatNumber(widget.mainValue);
+  }
+
+  TextStyle mainValueStyle() => TextStyle(
+        fontSize: 45.0,
+        letterSpacing: 1.0,
+        fontWeight: FontWeight.bold,
+      );
 }
