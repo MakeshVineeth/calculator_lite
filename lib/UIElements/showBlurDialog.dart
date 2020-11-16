6d0842638bf27edc9391c55978f753e9776c8bdc
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-11-16 14:42:19 +0530

    New Blur implement

--- lib/UIElements/showBlurDialog.dart
+++ lib/UIElements/showBlurDialog.dart
@@ -2,6 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:calculator_lite/fixedValues.dart';
 import 'dart:ui';
 
+class BlurredWindow extends StatefulWidget {
+  final Widget child;
+
+  const BlurredWindow({@required this.child});
+  @override
+  _BlurredWindowState createState() => _BlurredWindowState();
+}
+
+class _BlurredWindowState extends State<BlurredWindow> {
+  double _currentOpacity = 0;
+  final Duration duration = const Duration(milliseconds: 1000);
+  Widget _currentChild;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _currentOpacity = 1;
+      _currentChild = BackdropFilter(
+        filter: ImageFilter.blur(
+            sigmaX: FixedValues.sigmaLevel, sigmaY: FixedValues.sigmaLevel),
+        child: widget.child,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => AnimatedOpacity(
+        key: UniqueKey(),
+        opacity: _currentOpacity,
+        duration: duration,
+        child: GestureDetector(
+          onTap: () => Navigator.of(context, rootNavigator: true).pop(),
+          child: AnimatedContainer(
+            key: UniqueKey(),
+            duration: duration,
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            child: _currentChild,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future<Type> showBlurDialog(
+    {@required BuildContext context, @required Widget child}) {
+  return showDialog(
+    context: context,
+    barrierColor: Colors.transparent,
+    barrierDismissible: true,
+    builder: (context) => BlurredWindow(
+      child: child,
+    ),
+  );
+}
+
+/* ORIGINAL BLUR IMPLEMENTATION
+
 Future<Type> showBlurDialog(
     {@required BuildContext context, @required Widget child}) {
   return showGeneralDialog(
@@ -25,3 +88,5 @@ Future<Type> showBlurDialog(
     barrierLabel: '',
   );
 }
+
+*/
