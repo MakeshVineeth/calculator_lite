795e6043c7bbc4a82255a4cb35e84a97e5441b88
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-12-06 15:04:58 +0530

    Changes to currencyListItem.

--- lib/CurrencyTab/Backend/currencyListItem.dart
+++ lib/CurrencyTab/Backend/currencyListItem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:country_provider/country_provider.dart';
+import 'country_by_currency.dart';
 
 class CurrencyListItem {
   final String currencyCode;
@@ -23,9 +24,30 @@ class CurrencyListItem {
       List<Currency> currencies = result.elementAt(i).currencies;
 
       for (int i = 0; i < currencies.length; i++) {
+        // Check if currency code == given one.
         Currency current = currencies.elementAt(i);
         if (current.code == currencyCode) {
           currencyName = current.name;
+
+          // Get full name using json and then get alpha2 code.
+          for (int i = 0; i < countryJsonList.length; i++) {
+            String iterateValue = countryJsonList.values
+                .elementAt(i)
+                .toString()
+                .trim()
+                .toLowerCase();
+            String toCompareValue = currencyName.trim().toLowerCase();
+            if (iterateValue == toCompareValue) {
+              String countryName = countryJsonList.keys.elementAt(i);
+              Country countryObj =
+                  await CountryProvider.getCountryByFullname(countryName);
+              flagURL =
+                  'icons/flags/${countryObj.alpha2Code.toLowerCase().trim()}.png';
+              print(flagURL);
+              break;
+            }
+          }
+
           isChecked = true;
           break;
         }
@@ -33,6 +55,7 @@ class CurrencyListItem {
         if (isChecked) break;
       }
 
+      // breaking all loops as once we have all values, we don't have to iterate anymore.
       if (isChecked) break;
     }
   }
