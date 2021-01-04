c92b1be5839e049477d071e663347b8687835156
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-01-04 11:07:56 +0530

    Change to ImageFiltered

--- lib/UIElements/showBlurDialog.dart
+++ lib/UIElements/showBlurDialog.dart
@@ -21,8 +21,8 @@ class _BlurredWindowState extends State<BlurredWindow> {
 
     setState(() {
       _currentOpacity = 1;
-      _currentChild = BackdropFilter(
-        filter: ImageFilter.blur(
+      _currentChild = ImageFiltered(
+        imageFilter: ImageFilter.blur(
             sigmaX: FixedValues.sigmaLevel, sigmaY: FixedValues.sigmaLevel),
         child: widget.child,
       );
