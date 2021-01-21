3fd12cc7c1910f93fc0fb4304a679e6552e48e01
Makesh Vineeth <makeshvineeth1997@gmail.com>
2021-01-21 16:37:58 +0530

    Fix render issues with slideUp

--- lib/UIElements/showSlideUp.dart
+++ lib/UIElements/showSlideUp.dart
@@ -5,29 +5,23 @@ import 'package:calculator_lite/fixedValues.dart';
 void showSlideUp(
     {@required BuildContext context,
     @required Map<String, Function> menuList}) {
-  double height = MediaQuery.of(context).size.height;
-
   slideDialog.showSlideDialog(
     context: context,
-    child: SingleChildScrollView(
-      physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-      child: Container(
-        height: height / 2,
-        child: ListView.separated(
-          cacheExtent: 2000,
-          addAutomaticKeepAlives: true,
-          addRepaintBoundaries: true,
-          physics: NeverScrollableScrollPhysics(),
-          separatorBuilder: (context, index) => Divider(thickness: 1),
-          itemCount: menuList.length,
-          itemBuilder: (context, index) => buttonDesigned(
-            function: () {
-              Navigator.of(context, rootNavigator: true).pop();
-              menuList.entries.elementAt(index).value();
-            },
-            text: menuList.entries.elementAt(index).key,
-            context: context,
-          ),
+    child: Expanded(
+      child: ListView.separated(
+        cacheExtent: 2000,
+        addAutomaticKeepAlives: true,
+        addRepaintBoundaries: true,
+        physics: AlwaysScrollableScrollPhysics(parent: BouncingScrollPhysics()),
+        separatorBuilder: (context, index) => Divider(thickness: 1),
+        itemCount: menuList.length,
+        itemBuilder: (context, index) => buttonDesigned(
+          function: () {
+            Navigator.of(context, rootNavigator: true).pop();
+            menuList.entries.elementAt(index).value();
+          },
+          text: menuList.entries.elementAt(index).key,
+          context: context,
         ),
       ),
     ),
@@ -35,31 +29,32 @@ void showSlideUp(
 }
 
 Widget buttonDesigned(
-        {@required Function function,
-        @required String text,
-        @required BuildContext context}) =>
-    Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-      child: TextButton(
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all(FixedValues.roundShapeLarge),
-        ),
-        onPressed: () => function(),
-        child: IgnorePointer(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Center(
-              child: Text(
-                text,
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18,
-                  letterSpacing: 0.5,
-                ),
+    {@required Function function,
+    @required String text,
+    @required BuildContext context}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+    child: TextButton(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(FixedValues.roundShapeLarge),
+      ),
+      onPressed: () => function(),
+      child: IgnorePointer(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+                letterSpacing: 0.5,
               ),
             ),
           ),
         ),
       ),
-    );
+    ),
+  );
+}
